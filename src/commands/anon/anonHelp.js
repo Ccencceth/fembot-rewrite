d987bb37a2baa7a371edd1fa7a478ae79b2b423a
Ccencceth <45124495+Ccencceth@users.noreply.github.com>
2023-05-19 21:16:37 +0800

    anon help command

--- src/commands/anon/anonHelp.js
+++ src/commands/anon/anonHelp.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, Client } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder().setName("anonhelp").setDescription("help for all things anonymous messaging related."),
@@ -7,9 +7,21 @@ module.exports = {
       fetchReply: true,
     });
 
+    const helpEmbed = new EmbedBuilder()
+      .setColor(client.color)
+      .setTitle('Anon Message Help')
+      .setDescription('How t-to use da x3 fembot anyonymous messaging system.')
+      .addFields(
+        { name: '/anonhelp', value: 'omg hi this haiii :3'},
+        { name: '/setanonchannel', value: 'Sets the channel as the servers anonymous message channel. Only admins can use this command.'},
+        { name: '/anonmessage setdefault', value: 'This will give you a dropdown menu of all the servers you share with fembot. Choose the one you wish to send anonymous messages to. You can change this at any time :)'},
+        { name: '/anonmessage send', value: 'This allows you to send text and/or a file attachment (video/gif/whatever) anonymously to the selected servers anonymous channel.'},
+        { name: '/anonmessage dm', value: 'coming soon!!'},
+      )
+
     // embed for command explanations :D
     await interaction.editReply({
-        content: "Blehhh not done yet",
+        embeds: [helpEmbed],
     })
   },
 };
