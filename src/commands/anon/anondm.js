1b9448388171147b73911215623533549c82c83d
Ccencceth <45124495+Ccencceth@users.noreply.github.com>
2023-06-07 20:13:47 +0800

    dms toggle made

--- src/commands/anon/anondm.js
+++ src/commands/anon/anondm.js
@@ -6,17 +6,61 @@ module.exports = {
     .setName("anondm")
     .setDescription("anonymous dms")
     .addSubcommand((subcommand) =>
-      subcommand.setName("toggle").setDescription("enable/disable recieving anonymous dms")
+      subcommand
+        .setName("toggle")
+        .setDescription("enable/disable recieving anonymous dms")
     )
     .addSubcommand((subcommand) =>
       subcommand.setName("send").setDescription("send an anonymous dm")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("blockuser")
+        .setDescription("block a specific person you HATE from sending anondms")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("unblockuser")
+        .setDescription(
+          "unblock a specific person you LOVE from sending anondms"
+        )
     ),
   async execute(interaction) {
+    let userProfile = await anonUserSettings.findOne({
+      _id: interaction.user.id,
+    });
+
+    if (!userProfile) {
+      userProfile = await new anonUserSettings({
+        _id: interaction.user.id,
+        defaultAnonServerId: "none",
+        blockAll: true,
+        blockedUsers: [],
+      });
+    }
+
     if (interaction.options.getSubcommand() === "send") {
       await interaction.reply("working on it");
       return;
-    } 
+    }
     if (interaction.options.getSubcommand() === "toggle") {
+      await interaction.reply("working on it");
+
+      userProfile.blockAll = !userProfile.blockAll;
+      userProfile.save().catch(console.error);
+
+      if (userProfile.blockAll) {
+        await interaction.editReply("You can no longer receive anonymous dms");
+      } else {
+        await interaction.editReply("You can now receive anonymous dms");
+      }
+      return;
+    }
+    if (interaction.options.getSubcommand() === "blockuser") {
+      await interaction.reply("working on it");
+      return;
+    }
+    if (interaction.options.getSubcommand() === "unblockuser") {
       await interaction.reply("working on it");
       return;
     }
